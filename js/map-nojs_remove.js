c2b7ea995cdc74e6c1297f7972a5589c51052eca
denispan <34074159+denispan@users.noreply.github.com>
2025-02-14 13:43:15 +0000

    Deploying to gh-pages from @ denispan/cat-energy-shop@f3ea7f97f8c6213727841497ba300940bce5ad18 🚀

---
+++ js/map-nojs_remove.js
@@ -0,0 +1,5 @@
+const mapView = document.querySelector('.map__view');
+const mapImage = document.querySelector('.map__image');
+
+mapView.classList.remove('map__view--nojs');
+mapImage.classList.remove('map__image--nojs');
